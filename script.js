e05affcde4935ea3c33bff1d93651a3d7a6f994b
Vicente Guzman <luciomsp@geeks.ms>
2022-08-18 11:25:51 -0500

    Update

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ const PIPE_SPEED = 90
 
 loadRoot("/sprites/")
 loadSprite("bird", "bird.png")
-loadSprite("background", "ocean.jpg")
-loadSprite("gameover_bg", "bird_bg.jpg")
+loadSprite("background", "background.png")
+loadSprite("gameover_bg", "gameOver.jpg")
 loadSprite("pipe", "pipe.png")
 
 scene("main", () => {
